728234a720fa76545452390eb1c2516435a54061
Vagner ferreira <euvagnerferreira@gmail.com>
2024-05-05 16:02:47 -0300

    Validando o objeto UsuarioCreateDto

--- src/main/java/com/mballem/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/com/mballem/demoparkapi/web/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import com.mballem.demoparkapi.web.dto.UsuarioCreateDto;
 import com.mballem.demoparkapi.web.dto.UsuarioResponseDto;
 import com.mballem.demoparkapi.web.dto.UsuarioSenhaDto;
 import com.mballem.demoparkapi.web.dto.mapper.UsuarioMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +22,7 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @PostMapping
-    public ResponseEntity<UsuarioResponseDto> create(@RequestBody UsuarioCreateDto createDto){
+    public ResponseEntity<UsuarioResponseDto> create(@Valid @RequestBody UsuarioCreateDto createDto){
         Usuario user = usuarioService.salvar(UsuarioMapper.toUsuario(createDto));
         return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
     }
--- src/main/java/com/mballem/demoparkapi/web/dto/UsuarioCreateDto.java
+++ src/main/java/com/mballem/demoparkapi/web/dto/UsuarioCreateDto.java
@@ -1,11 +1,18 @@
 package com.mballem.demoparkapi.web.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class UsuarioCreateDto {
 
+    @NotBlank
+    @Email(message = "formato do email invalido", regexp = "^[a-z0-0.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
     private String username;
+    @NotBlank
+    @Size(min = 6, max = 6)
     private String password;
 
 }
