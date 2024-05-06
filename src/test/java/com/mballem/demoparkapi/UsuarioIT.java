6e3e7b8c7e56f3effb54c71ee8546b0097733b86
Vagner ferreira <euvagnerferreira@gmail.com>
2024-05-06 16:34:30 -0300

    Teste ponto a ponto para testar exceção de conflito (409)

--- src/test/java/com/mballem/demoparkapi/UsuarioIT.java
+++ src/test/java/com/mballem/demoparkapi/UsuarioIT.java
@@ -119,4 +119,20 @@ public class UsuarioIT {
         org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
         org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
     }
+    @Test
+    public void createUsuario_ComUsernameRepetido_RetornarErrorMessageomStatus409(){
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/usuarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UsuarioCreateDto("ana@gmail.com", "123456"))
+                .exchange()
+                .expectStatus().isEqualTo(409)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(409);
+
+    }
 }
